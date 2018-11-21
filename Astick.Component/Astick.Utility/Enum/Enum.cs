9eb74a5166acb609525f43c06764f397022c3ed9
slddlhr <120812424@qq.com>
2018-11-21 22:42:45 +0800

    utility.enum-1

---
+++ Astick.Component/Astick.Utility/Enum/Enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astick.Utility.Enum
+{
+    /// <summary>
+    /// 泛型枚举帮助类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Enum<T> where T:struct
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<T> AsEnumrable()
+        {
+
+        }
+    }
+}
